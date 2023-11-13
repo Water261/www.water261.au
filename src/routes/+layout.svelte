0e4b579e03d5547291b6d43c138308076c9090c7
Matthew Wilks <water261@outlook.com.au>
2023-11-13 12:35:54 +1000

    Update deps & add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,16 @@
 	import "../app.postcss";
 </script>
 
-<slot />
+<nav class="navbar shadow z-10 bg-base-100">
+	<div class="flex-1">
+		<a href="#top" class="btn btn-ghost normal-case text-xl">Matthew Wilks</a>
+		<div class="divider divider-horizontal mx-0" />
+		<ul class="menu menu-horizontal py-0">
+			<li><a href="https://github.com/Water261" rel="noreferrer" class="text-xl">GitHub</a></li>
+		</ul>
+	</div>
+</nav>
+
+<main class="z-0">
+	<slot />
+</main>	
